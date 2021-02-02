4de17181489ab45fdae9aedf7a13f828f0fb6789
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2021-02-02 12:51:25 +0100

    refactor(core): fixs

--- tests/Messenger/TaskMessageHandlerTest.php
+++ tests/Messenger/TaskMessageHandlerTest.php
@@ -28,7 +28,23 @@ final class TaskMessageHandlerTest extends TestCase
 
         $worker = $this->createMock(WorkerInterface::class);
         $worker->expects(self::once())->method('isRunning')->willReturn(false);
-        $worker->expects(self::once())->method('execute');
+        $worker->expects(self::once())->method('execute')->with([], $task);
+
+        $handler = new TaskMessageHandler($worker);
+
+        ($handler)(new TaskMessage($task));
+    }
+
+    public function testHandlerCanWaitForAvailableWorker(): void
+    {
+        $task = new ShellTask('foo', ['echo', 'Symfony']);
+        $task->setScheduledAt(new DateTimeImmutable());
+        $task->setExpression('* * * * *');
+        $task->setTimezone(new DateTimeZone('UTC'));
+
+        $worker = $this->createMock(WorkerInterface::class);
+        $worker->expects(self::exactly(3))->method('isRunning')->willReturnOnConsecutiveCalls(true, true, false);
+        $worker->expects(self::once())->method('execute')->with([], $task);
 
         $handler = new TaskMessageHandler($worker);
 
