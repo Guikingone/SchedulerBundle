e715e704aec0933cdd6f87a6bf605d77fdf66ef4
babeuloula <babeuloula@gmail.com>
2021-04-23 09:48:29 +0200

    tests(expression): different timezone

--- tests/Expression/FluentExpressionBuilderTest.php
+++ tests/Expression/FluentExpressionBuilderTest.php
@@ -33,12 +33,12 @@ final class FluentExpressionBuilderTest extends TestCase
     }
 
     /**
-     * @dataProvider provideExpression
+     * @dataProvider provideExpressionWithTimezone
      */
-    public function testBuilderCanBuildWithTimezone(string $expression, string $endExpression): void
+    public function testBuilderCanBuildWithTimezone(string $expression, string $endExpression, string $timezone): void
     {
         $fluentExpressionBuilder = new FluentExpressionBuilder();
-        $finalExpression = $fluentExpressionBuilder->build($expression);
+        $finalExpression = $fluentExpressionBuilder->build($expression, $timezone);
 
         self::assertSame($finalExpression->getExpression(), $endExpression);
     }
@@ -50,5 +50,31 @@ final class FluentExpressionBuilderTest extends TestCase
         yield ['first day of January 2008', '0 0 1 1 2'];
         yield ['12/22/78', '0 0 22 12 5'];
         yield ['10/Oct/2000:13:55:36 -0700', '55 20 10 10 2'];
+    }
+
+    public function provideExpressionWithTimezone(): Generator
+    {
+        yield ['first monday of January 1980 10:00', '0 10 7 1 1', 'UTC'];
+        yield ['last monday of July 1970 10:00', '0 10 27 7 1', 'UTC'];
+        yield ['first day of January 2008', '0 0 1 1 2', 'UTC'];
+        yield ['12/22/78', '0 0 22 12 5', 'UTC'];
+        yield ['10/Oct/2000:13:55:36 -0700', '55 20 10 10 2', 'UTC'];
+
+        // Test with a different timezone
+        $datetime = new \DateTimeImmutable();
+        $datetime = $datetime->setTimezone(new \DateTimeZone('Europe/Paris'));
+        $datetime = $datetime->modify("+1 minute");
+        yield [
+            $datetime->format(\DateTimeImmutable::RFC3339),
+            sprintf(
+                '%d %s %s %s %s',
+                (int) $datetime->format('i'),
+                $datetime->format('G'),
+                $datetime->format('j'),
+                $datetime->format('n'),
+                $datetime->format('w')
+            ),
+            'Europe/Paris',
+        ];
     }
 }
